e8ee758de914f02781e82037798f0e0be57a6f36
kennytancl101223727 <101223727@students.swinburne.edu.my>
2021-04-29 17:11:04 +0800

    fix php

--- reg_check.php
+++ reg_check.php
@@ -6,16 +6,16 @@
 </head>
 
 <body>
-    <?php
+<?php
     // echo $_POST['input_nric'];
     if (file_exists($_POST['input_nric'] . '.txt')) {
         echo '<h3>You are already registered!</h3>';
-    } 
+    }
     else {
         echo '<h3>You have not registered!</h3><br>';
         echo '<a href ="register.html">Register here</a>';
     }
-    ?>
+?>
    
 </body>
 </html>
