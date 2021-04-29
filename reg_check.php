cd45c0616938eae478e6455cef6b6d3aaf25b04a
kennytancl101223727 <101223727@students.swinburne.edu.my>
2021-04-29 17:04:14 +0800

    edited header

--- reg_check.php
+++ reg_check.php
@@ -9,8 +9,8 @@
     <?php
     // echo $_POST['input_nric'];
     if (file_exists($_POST['input_nric'] . '.txt')) {
-        echo '<h3>You are already registered!</h3>'; 
-    }
+        echo '<h3>You are already registered!</h3>';
+    } 
     else {
         echo '<h3>You have not registered!</h3><br>';
         echo '<a href ="register.html">Register here</a>';
--- reg_process.php
+++ reg_process.php
@@ -12,8 +12,8 @@
     // echo $_POST['input_nric'];
     if (file_exists($_POST['input_nric'] . '.txt')) {
         //Check for filename with given nric
-        echo '<h3>You are already registered!</h3>'; 
-    }
+        echo '<h3>You are already registered!</h3>';
+    } 
     else {
         //filename not found, write to file        
         $content_to_write = "Name: " . $_POST['input_name'] . "\n";
