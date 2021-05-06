2c1c92499b93f3ed2b4f9a3559e9793cd2220ca9
Adrian Sim Huan Tze <101225244@students.swinburne.edu.my>
2021-05-06 10:41:27 +0800

    Load footer.php for all php files

--- reg_process.php
+++ reg_process.php
@@ -103,68 +103,7 @@
         </main>
     </div>
 
-    <div class ="container-fluid site-title footer">
-        <footer>
-            <table id="footer-table">
-                <tr>
-                    <th>
-                        Enquiries
-                    </th>
-                    <th>
-                        Relative Information
-                    </th>
-                    <th>
-                        Connect With Us
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                        Visit Us
-                    </td>
-                    <td>
-                        <a href="home.html">COVID-19 Info</a>
-                    </td>
-                    <td rowspan="5">
-                        <img src="images/connect_us.png" alt="Clipboard with checkmarks"
-                        class="card-img-top img-fluid p-0 bg-primary card-img-orig d-none d-md-block rounded-0">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Call 03-234-4553
-                    </td>
-                    <td>
-                        <a href="page1.html">Pfizer-BioNTech</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <a href="mailto:COVID_INFO@gmail.com">Email Us</a>
-                    </td>
-                    <td>
-                        <a href="page2.html">AstraZeneca</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Open 24/7
-                    </td>
-                    <td>
-                        <a href="page3.html">Sinovac</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Locate Us
-                    </td>
-                    <td>
-                        <a href="page3.html">Sputnik</a>
-                    </td>
-                </tr>
-            </table>
-            <p>&#169;2021 Development Project Tools 1 Group 7</p>
-        </footer>
-    </div>
+    <?php include 'footer.php';?>
    
 </body>
 </html>
